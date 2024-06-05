e70a18b38f26a0701b50c222c52556af63f692cd
a201a <115426699+a201a@users.noreply.github.com>
2024-06-05 07:59:24 +0000

    update

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import os
 import telebot
-
+from flask import Flask
 import config  # يجب وجود ملف config.py في نفس المجلد
 
+app = Flask(__name__)
 bot = telebot.TeleBot(config.TOKEN)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 # تنفيذ الأوامر الموجودة في المجلد commands
 for filename in os.listdir('./commands'):
     if filename.endswith('.py'):
         exec(open(f'./commands/{filename}').read())
 
-bot.polling()
+# بدء تشغيل بوت تليجرام في خيط منفصل
+import threading
+telegram_thread = threading.Thread(target=bot.polling)
+telegram_thread.start()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
